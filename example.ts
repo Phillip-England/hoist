c1616ec78fc365a752aace693ca8d25eb883940e
Phillip-England <phillip.mark.england@gmail.com>
2025-10-16 19:58:27 -0500

    ensured I could use absolutePaths as keys

--- example.ts
+++ example.ts
@@ -1,17 +1,18 @@
+import path from 'path';
 import { VirtualAsset } from "./src/VirtualAsset";
 import { VirtualFS } from './src/VirtualFS';
 
-let newFs = await VirtualFS.overwrite('./cli',{
-  './cli': VirtualAsset.rootDir(),
-  './cli/index.html': VirtualAsset.file(`<h1>Hello, World!</h1>`)
+let newFs = await VirtualFS.overwrite(path.join(process.cwd(), 'cli'),{
+  [path.join(process.cwd(), 'cli')]: VirtualAsset.rootDir(),
+  [path.join(process.cwd(), 'cli', 'index.html')]: VirtualAsset.file(`<h1>Hello, World!</h1>`)
 })
-let newText = newFs.read('./cli/index.html')
+let newText = newFs.read(path.join(process.cwd(), 'cli', 'index.html'))
 console.log(newText)
 
-let loadedFs = await VirtualFS.load('./cli')
-let loadedText = loadedFs.read('./cli/index.html')
+let loadedFs = await VirtualFS.load(path.join(process.cwd(), 'cli'))
+let loadedText = loadedFs.read(path.join(process.cwd(), 'cli', 'index.html'))
 console.log(loadedText)
 
-loadedFs.write('./cli/index.html', `<h1>Hello, Nobody!</h1>`)
+loadedFs.write(path.join(process.cwd(), 'cli', 'index.html'), `<h1>Hello, Nobody!</h1>`)
 await loadedFs.sync()
-console.log(loadedFs.read('./cli/index.html'))
+console.log(loadedFs.read(path.join(process.cwd(), 'cli', 'index.html')))
